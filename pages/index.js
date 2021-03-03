2b6e230ffc32b8ff91e597ab940fea281bc20846
wilberpulido <pulidowilber@gmail.com>
2021-03-03 13:23:15 -0300

    agregue hat_normal

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,45 @@ export default function Home() {
         <title>Monopoly</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1> Monopoly</h1>
+      <header>
+      </header>
+      <main>
+        <section>
+          <figure>
+            <img src="/images/hat_normal.png" alt="hat-monopoly"/>
+            <h1> In construction. . .</h1>
+          </figure>
+        </section>
+        <section>
+          <div>
+            <div>
+              <h3>
+                P
+              </h3>
+            </div>
+            <div>
+              <h2>
+                WILBER
+              </h2>
+            </div>
+            <div>
+              <h3>
+                P
+              </h3>
+            </div>
+            <div>
+              <h2>
+                GABRIEL
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section>
+          <a href="https://github.com/The-Best-String/Monopoly"> Github proyect</a>
+        </section>
+      </main>
+      <footer>
+      </footer>
     </div>
   )
 }
