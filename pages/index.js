fc6f7a815bd81c0aa52c94df386a3811d6cc2dee
Wilber Pulido <41765053+wilberpulido@users.noreply.github.com>
2021-03-03 19:07:56 -0300

    MON-1
    * agregue hat_normal
    
    * MON-1 Fin de la estructura propuesta

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,49 @@ export default function Home() {
         <title>Monopoly</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1> Monopoly</h1>
+      <header>
+      </header>
+      <main>
+        <section>
+          <figure>
+            <img src="/images/hat_normal.png" alt="hat-monopoly"/>
+          </figure>
+            <h1> In construction. . .</h1>
+        </section>
+        <section>
+          <div>
+            <div>
+              <h3>
+                P
+              </h3>
+            </div>
+            <div>
+              <h2>
+                WILBER
+              </h2>
+            </div>
+            <div>
+              <h3>
+                P
+              </h3>
+            </div>
+            <div>
+              <h2>
+                GABRIEL
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section>
+            <div>
+              <a href="https://github.com/The-Best-String/Monopoly" target="_blank">
+                Github proyect
+              </a>
+            </div>
+        </section>
+      </main>
+      <footer>
+      </footer>
     </div>
   )
 }
